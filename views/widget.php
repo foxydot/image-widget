9a4710de3d08df88a08bbe95e8771e4b1e95d85b
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-08-02 23:00:23 -0400

    update view

--- views/widget.php
+++ views/widget.php
@@ -6,18 +6,23 @@
 
 // Block direct requests
 if ( !defined('ABSPATH') )
-	die('-1');
+    die('-1');
 
 echo $before_widget;
 
 if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
-
+echo '<div class="widget-content">';
 echo $this->get_image_html( $instance, true );
 
 if ( !empty( $description ) ) {
-	echo '<div class="'.$this->widget_options['classname'].'-description" >';
-	echo wpautop( $description );
-	echo "</div>";
+    echo '<div class="'.$this->widget_options['classname'].'-description" >';
+    echo wpautop( $description );
+    echo "</div>";
 }
+if ( $link ) {
+    $linktext = $linktext != ''?$linktext:'Read More >';
+    echo '<div class="link"><a class="'.$this->widget_options['classname'].'-link readmore" href="'.$link.'" target="'.$linktarget.'">'.$linktext.' ></a><div class="clear"></div></div>';
+}
+echo '<div class="clear"></div>
+</div>';
 echo $after_widget;
-?>
